2c484aa3783c7cb032c6db249037a34c7531fd63
Faisal Ahmmad <faisal.ahmmad@ersg-global.com>
2022-05-12 19:04:09 +0100

    Removed unnecessary variables

--- index.php
+++ index.php
@@ -4,10 +4,12 @@ use Classes\PurchaseManager;
 require_once './autoloader.php';
 
 $purchaseManager = new PurchaseManager();
-$cart = $purchaseManager->purchase('A', 2);
-$cart = $purchaseManager->purchase('D', 1);
+
+$purchaseManager->purchase('A', 2);
+$purchaseManager->purchase('D', 1);
 $purchaseManager->applyPromotion();
 
 $purchaseManager->render();
+
 echo "Total:".$purchaseManager->total()."\n";
 
